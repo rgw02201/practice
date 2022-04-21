e6a962b75e757b0fa7e2bab67a142a5a151dccb8
rgw02201 <rgw02201@naver.com>
2022-04-21 11:36:58 +0900

    1.0 Initialization

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2b7dbVK-ntgM4gxF0Du6wW52DQ-qAOtk",
+  authDomain: "nwitter-191c1.firebaseapp.com",
+  projectId: "nwitter-191c1",
+  storageBucket: "nwitter-191c1.appspot.com",
+  messagingSenderId: "914576185107",
+  appId: "1:914576185107:web:c73c6e0eec5911a3b62ce6",
+};
+
+export default firebase.initializeApp(firebaseConfig);
